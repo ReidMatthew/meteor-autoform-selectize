1940f00a56dfc1cf92c8238912dd3decb60b3d75
Danail Gabenski <dgabenski@fortressinfosec.com>
2025-05-21 15:07:01 -0600

    Updated for Meteor 3.x
    Updated version to 2.3.4 from 2.3.3
    Updated versions of dependencies to ones for Meteor 3.2.2
    Updated ' to "
    Reformatted

--- package.js
+++ package.js
@@ -1,21 +1,23 @@
 Package.describe({
-  name: 'ReidMatthew:autoform-selectize',
-  summary: 'Custom "selectize" input type for AutoForm',
-  version: '2.3.3',
-  git: 'https://github.com/ReidMatthew/meteor-autoform-selectize.git'
+  name    : "reidmatthew:autoform-selectize",
+  summary : "Custom 'selectize' input type for AutoForm",
+  version : "2.3.4",
+  git     : "https://github.com/reidmatthew/meteor-autoform-selectize.git"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0');
-  api.use('templating@1.0.0');
-  api.use('blaze@2.0.0');
-  api.use('aldeed:autoform@6.0.0');
-  api.use('underscore@1.0.0');
+  api.versionsFrom("METEOR@3.2.2");
+
+  api.use("templating@1.4.4");
+  api.use("blaze@3.0.2");
+  api.use("aldeed:autoform@8.0.0");
+  api.use("underscore@1.6.4");
+
   api.addFiles([
-    'autoform-selectize.html',
-    'autoform-selectize.js',
-    'autoform-selectize-input.html',
-    'autoform-selectize-input.js',
-    'themes/bootstrap3.css',
-  ], 'client');
+    "autoform-selectize.html",
+    "autoform-selectize.js",
+    "autoform-selectize-input.html",
+    "autoform-selectize-input.js",
+    "themes/bootstrap3.css",
+  ], "client");
 });
